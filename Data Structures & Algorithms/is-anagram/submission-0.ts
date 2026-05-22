430c83d5195ed2e3acaf450f4e1086cfbc6c3220
Alex Brylov <alexbrylov1994@hotmail.com>
2026-05-21 21:35:16 -0600

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+    if (s.length != t.length) {
+        return false;
+    }
+    let hashS = {};
+    for (let char of s) {
+        if (hashS[char]) {
+            hashS[char] += 1
+        } else {
+            hashS[char] = 1
+        }
+    }
+    for (let char of t) {
+        if (hashS[char]) {
+            hashS[char] -= 1
+        } else {
+            return false
+        }
+    }
+    return true;
+    }
+}
