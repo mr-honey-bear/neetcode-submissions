f73151544573d91107ab02576bdccb6500ef6e2a
Alex Brylov <alexbrylov1994@hotmail.com>
2026-05-21 22:19:29 -0600

    Add: longest-consecutive-sequence - submission-8

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-8.js
@@ -0,0 +1,36 @@
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+
+        let set = new Set(nums.sort((a, b) => a - b));
+        let sorted = [...set];
+
+        let result = [];
+        let temp = new Set();
+
+        for (let i = 0; i < sorted.length; i++) {
+            if (i != sorted.length - 1) {
+                if (sorted[i] == (sorted[i + 1] - 1)) {
+                    temp.add(sorted[i]);
+                    temp.add(sorted[i + 1]);
+
+                    if (result.length < temp.size) {
+                        result = [...temp];
+                    }
+                } else {
+                    temp.clear();
+                }
+            }
+        }
+
+        if (result.length === 0 && sorted.length > 0) {
+            return 1;
+        }
+
+        return result.length;
+    }
+}
