ac1c3a6d6dc6e61dc5aae879ca09d1c7423edb0e
Alex Brylov <alexbrylov1994@hotmail.com>
2026-05-21 21:38:22 -0600

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let hashMap = {}
+    
+    for (let num of nums) {
+        if (num in hashMap) {
+            hashMap[num] += 1; 
+        } else {
+            hashMap[num] = 1; 
+        }
+    }
+    let arr = Object.keys(hashMap).sort((a,b) => hashMap[b] - hashMap[a])
+    return arr.slice(0, k).map(Number);
+    }
+}
