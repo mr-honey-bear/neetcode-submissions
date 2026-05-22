09b38ed409f510c28d15be2a8a4cc2d3a515d443
Alex Brylov <alexbrylov1994@hotmail.com>
2026-05-21 21:43:38 -0600

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        let hashMap = {};
+        for (let i=0; i < nums.length; i++) {
+            let targetNumber = target - nums[i];
+            // or hashMap[targetNumber) !== undefined
+            if (targetNumber in hashMap) {
+                return [i, hashMap[targetNumber]]
+            } else {
+                hashMap[nums[i]] = i
+            }
+        }
+        return [];
+    };
+}
