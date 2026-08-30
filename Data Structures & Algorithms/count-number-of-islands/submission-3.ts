370b54d597aaf382cf4468b0aee5aaec5233a1a3
Alex Brylov <alexbrylov1994@hotmail.com>
2026-08-30 00:11:57 -0600

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.ts
@@ -0,0 +1,50 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid: string[][]): number {
+
+    let dirs = [
+        [-1,0],
+        [0,1],
+        [1,0],
+        [0,-1]
+    ]
+
+    let islands = 0;
+
+    for (let row=0; row < grid.length; row++) {
+        for (let col=0; col < grid[0].length; col++) {
+            if (grid[row][col] === "1") {
+                grid[row][col] = "0"
+                islands++
+
+                let dfs = (point) => {
+
+                    for (let dir of dirs) {
+                            let nextRow = point[0] + dir[0];
+                            let nextCol = point[1] + dir[1];
+
+                            if (
+                                nextRow >= 0 && 
+                                nextRow < grid.length && 
+                                nextCol >= 0 && 
+                                nextCol < grid[0].length &&
+                                grid[nextRow][nextCol] === "1"
+                            ) {
+                                grid[nextRow][nextCol] = "0";
+                                dfs([nextRow, nextCol]);
+                            }
+                        }
+
+                }
+
+                dfs([row,col]);
+            }
+        }
+    }
+    
+    return islands;
+    }
+}
