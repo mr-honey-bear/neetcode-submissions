e1134e11eba4492316da62b6eb83d0177a77dc1e
Alex Brylov <alexbrylov1994@hotmail.com>
2026-09-01 21:09:09 -0600

    Add: count-connected-components - submission-11

---
+++ Data Structures & Algorithms/count-connected-components/submission-11.ts
@@ -0,0 +1,52 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n: number, edges: number[][]): number {
+        if (n <= 1) {
+            return n;
+        }
+
+        let graph = {};
+        let visited = new Set();
+        let num = 0;
+    
+        for (let i = 0; i < n; i++) {
+            graph[i] = []
+        }
+
+    for (let edge of edges) {
+        let currentNode = edge[0];
+        let nextNode = edge[1];
+
+        graph[currentNode].push(nextNode);
+        graph[nextNode].push(currentNode);
+    }
+
+        console.log(graph)
+
+        let dfs = (node) => {
+            for (let el of graph[node] || []) {
+                visited.add(node);
+                if (!visited.has(el)) {
+                    dfs(el)
+                }
+            }
+        }
+
+        for (let node = 0; node < n; node++) {
+            if (visited.has(node)) {
+                continue;
+            }
+            num++;
+
+
+            visited.add(node);
+            dfs(node)
+        }
+
+        return num;
+    }
+};
